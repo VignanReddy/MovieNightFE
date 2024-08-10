d1935ac1b37e940c24e315b9b9ebfb2594040d60
VignanReddy <37586434+VignanReddy@users.noreply.github.com>
2024-08-10 15:09:14 -0700

    client id

--- src/index.js
+++ src/index.js
@@ -9,11 +9,11 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <GoogleOAuthProvider clientId="701614447085-g6ancl9ogq50799qn4s88vds9fbtjklh.apps.googleusercontent.com">
+  <GoogleOAuthProvider clientId="466655036166-5ns31hedgv7vrafkj9brsi9273ijj3p6.apps.googleusercontent.com">
     <Router>
       <App />
     </Router>
   </GoogleOAuthProvider>
 );
 
-//466655036166-5ns31hedgv7vrafkj9brsi9273ijj3p6.apps.googleusercontent.com
+//701614447085-g6ancl9ogq50799qn4s88vds9fbtjklh.apps.googleusercontent.com
